67587e2e23aab5b21dd0645f1259b67409ac8e94
Tejas Tanna <56431505+tejasvtanna@users.noreply.github.com>
2025-03-17 13:29:22 +0530

    ProductsTable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { File, PlusCircle } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-import { ProductsTable } from '@/components/features/products/ProductsTable'
+import { Button } from '@/components/ui/Button'
+import { ProductsTable } from '../../components/features/products/ProductsTable'
 
 export default async function ProductsPage(props: {
   searchParams: Promise<{ q: string; offset: string }>
