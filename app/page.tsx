3bfa160ff20d03759fbab3daac6da98902bf642e
Tejas Tanna <56431505+tejasvtanna@users.noreply.github.com>
2025-03-17 13:13:20 +0530

    Remove (dashboard) route group

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { File, PlusCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
-import { ProductsTable } from '../../components/features/products/products-table';
+import { ProductsTable } from '../components/features/products/products-table';
 
 export default async function ProductsPage(props: {
   searchParams: Promise<{ q: string; offset: string }>;
