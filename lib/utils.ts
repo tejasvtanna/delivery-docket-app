5e084d33d25920edbfd8d11c00731b34b0d6acb9
Tejas Tanna <tejasvtanna@gmail.com>
2025-03-18 13:53:55 +0530

    wait function in util

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const wait = (ms = 3000) => {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
