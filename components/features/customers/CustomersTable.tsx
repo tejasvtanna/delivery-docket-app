107ab6eacf76ed99dbad1c0049042e05130cc171
Tejas Tanna <tejasvtanna@gmail.com>
2025-03-28 10:01:55 +0530

    Added customer search feature

--- components/features/customers/CustomersTable.tsx
+++ components/features/customers/CustomersTable.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useState, useEffect } from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { fetchXeroCustomers, XeroCustomer } from '@/actions/customer.actions'
 import {
@@ -10,41 +11,76 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table'
+import { Input } from '@/components/ui/input'
 
-export const CustomersTable = () => {
+export function CustomersTable() {
   const { data: customers = [] } = useQuery<XeroCustomer[]>({
     queryKey: ['xero-customers'],
     queryFn: fetchXeroCustomers
   })
 
+  const [searchText, setSearchText] = useState('')
+  const [filteredCustomers, setFilteredCustomers] = useState<XeroCustomer[]>([])
+
+  useEffect(() => {
+    const filtered = customers.filter((customer) => {
+      if (customer.contactID.toLowerCase().includes(searchText)) {
+        return true
+      }
+      if (customer.name.toLowerCase().includes(searchText)) {
+        return true
+      }
+      if (customer.contactStatus.toLowerCase().includes(searchText)) {
+        return true
+      }
+      if (customer.emailAddress?.toLowerCase().includes(searchText)) {
+        return true
+      }
+
+      return false
+    })
+    setFilteredCustomers(filtered)
+  }, [searchText, customers])
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>ID</TableHead>
-          <TableHead>Name</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {customers.length > 0 ? (
-          customers.map((customer) => (
-            <TableRow key={customer.contactID}>
-              <TableCell>{customer.contactID}</TableCell>
-              <TableCell>{customer.name}</TableCell>
-              <TableCell>{customer.emailAddress}</TableCell>
-              <TableCell>{customer.contactStatus}</TableCell>
-            </TableRow>
-          ))
-        ) : (
+    <>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-2xl font-bold mb-5'>Customers</h1>
+
+        <Input
+          onChange={(e) => setSearchText(e.target.value.toLowerCase())}
+          className='w-1/3 rounded'
+        />
+      </div>
+
+      <Table className='mt-2'>
+        <TableHeader>
           <TableRow>
-            <TableCell colSpan={3} className='text-center'>
-              No customers found
-            </TableCell>
+            <TableHead>ID</TableHead>
+            <TableHead>Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Status</TableHead>
           </TableRow>
-        )}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {filteredCustomers.length > 0 ? (
+            filteredCustomers.map((customer) => (
+              <TableRow key={customer.contactID}>
+                <TableCell>{customer.contactID}</TableCell>
+                <TableCell>{customer.name}</TableCell>
+                <TableCell>{customer.emailAddress}</TableCell>
+                <TableCell>{customer.contactStatus}</TableCell>
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={3} className='text-center'>
+                No customers found
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
   )
 }
