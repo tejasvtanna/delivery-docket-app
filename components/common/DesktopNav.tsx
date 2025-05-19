1c298381db5f902ee28179a3e51aa3e581f94344
Tejas Tanna <tejasvtanna@gmail.com>
2025-05-19 12:48:05 +0530

    Disable settings menu from nav

--- components/common/DesktopNav.tsx
+++ components/common/DesktopNav.tsx
@@ -39,7 +39,7 @@ export const DesktopNav = () => {
         </NavItem>
       </nav>
 
-      <nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
+      {/* <nav className='mt-auto flex flex-col items-center gap-4 px-2 sm:py-5'>
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
@@ -52,7 +52,7 @@ export const DesktopNav = () => {
           </TooltipTrigger>
           <TooltipContent side='right'>Settings</TooltipContent>
         </Tooltip>
-      </nav>
+      </nav> */}
     </aside>
   )
 }
